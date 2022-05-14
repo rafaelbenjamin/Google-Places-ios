09ca687e01be36567c6b7c29695a22a98ebbf23f
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-05-13 22:43:58 -0300

    Including separate Keys.plist to maintain api key

--- GooglePlacesManager.swift
+++ GooglePlacesManager.swift
@@ -25,7 +25,7 @@ final class GooglePlacesManager {
     }
     
     public func setUp(){
-        GMSPlacesClient.provideAPIKey("YOUR_API_KEY_HERE")
+        GMSPlacesClient.provideAPIKey(self.apiKey)
     }
     
     public func findPlaces(query: String,
@@ -51,4 +51,23 @@ final class GooglePlacesManager {
             
         }
     }
+}
+
+//MARK: - Get Api Keys from Keys.plist
+extension GooglePlacesManager {
+    private var apiKey: String {
+           get {
+               guard let filePath = Bundle.main.path(forResource: "Keys", ofType: "plist") else {
+                   fatalError("Couldn't find file 'Keys.plist'.")
+               }
+               let plist = NSDictionary(contentsOfFile: filePath)
+               guard let value = plist?.object(forKey: "GooglePlacesKey") as? String else {
+                   fatalError("Couldn't find key 'GooglePlacesKey' in 'Keys.plist'.")
+               }
+               if (value.starts(with: "_")) {
+                   fatalError("Register for a Google Places developer account and get an API key at https://developers.google.com/maps/documentation/places/ios-sdk/get-api-key")
+               }
+               return value
+           }
+       }
 }
